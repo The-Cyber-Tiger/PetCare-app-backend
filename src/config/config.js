463273f4c8483af12a209b3d374ec9f54cc4a3c6
The-Cyber-Tiger <rodrigo.gibran.dev@gmail.com>
2022-04-28 16:03:02 -0500

    env variables

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@
 
 export const config = {
     dbUrl: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9sp3u.mongodb.net/petcare-app?retryWrites=true&w=majority`,
-    serverPORT: '8000',
+    serverPORT: process.env.PORT || 8000,
     secrets: {
         jwt: process.env.JWT_SECRET,
         jwtExp: '1d'
